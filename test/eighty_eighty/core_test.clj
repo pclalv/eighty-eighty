92fa0a78f9468593555fe6b4eb98644c82d72a54
paul alvarez <palvarez@greenhouse.io>
2020-04-14 10:24:22 -0400

    Write tests over these complex fns

--- test/eighty_eighty/core_test.clj
+++ test/eighty_eighty/core_test.clj
@@ -2,6 +2,78 @@
   (:require [clojure.test :refer :all]
             [eighty-eighty.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest rlc-test
+  (testing "once"
+    (is (= {:flags {:cy 1}
+            :cpu {:a 2r01010101
+                  :pc 1}}
+           (-> {:flags {:cy 0}
+                :cpu {:a 2r10101010
+                      :pc 0}}
+               rlc))))
+  (testing "twice"
+    (is (= {:flags {:cy 0}
+            :cpu {:a 2r10101010
+                  :pc 2}}
+           (-> {:flags {:cy 0}
+                :cpu {:a 2r10101010
+                      :pc 0}}
+               rlc
+               rlc)))))
+
+(deftest rrc-test
+  (testing "once"
+    (is (= {:flags {:cy 1}
+            :cpu {:a 2r11000000
+                  :pc 1}}
+           (-> {:flags {:cy 0}
+                :cpu {:a 2r10000001
+                      :pc 0}}
+               rrc))))
+  (testing "twice"
+    (is (= {:flags {:cy 0}
+            :cpu {:a 2r01100000
+                  :pc 2}}
+           (-> {:flags {:cy 0}
+                :cpu {:a 2r10000001
+                      :pc 0}}
+               rrc
+               rrc)))))
+
+(deftest ral-test
+  (testing "once"
+    (is (= {:flags {:cy 1}
+            :cpu {:a 2r01010100
+                  :pc 1}}
+           (-> {:flags {:cy 0}
+                :cpu {:a 2r10101010
+                      :pc 0}}
+               ral))))
+  (testing "twice"
+    (is (= {:flags {:cy 0}
+            :cpu {:a 2r10101001
+                  :pc 2}}
+           (-> {:flags {:cy 0}
+                :cpu {:a 2r10101010
+                      :pc 0}}
+               ral
+               ral)))))
+
+(deftest rar-test
+  (testing "once"
+    (is (= {:flags {:cy 1}
+            :cpu {:a 2r01000000
+                  :pc 1}}
+           (-> {:flags {:cy 0}
+                :cpu {:a 2r10000001
+                      :pc 0}}
+               rar))))
+  (testing "twice"
+    (is (= {:flags {:cy 0}
+            :cpu {:a 2r10100000
+                  :pc 2}}
+           (-> {:flags {:cy 0}
+                :cpu {:a 2r10000001
+                      :pc 0}}
+               rar
+               rar)))))
